2153c6d1bae3df388b006908ddcbdb939171bc0a
John Cameron <john.n.cameron@gmail.com>
2024-12-10 08:35:25 +1000

    cqrs setup

---
+++ Services/Catalog/Products/CreateProduct/CreateProductEndpoint.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace Catalog.Products.CreateProduct;
+
+public class CreateProductEndpoint
+{
+
+}
---
+++ Services/Catalog/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,16 @@
+using BuildingBlocks.CQRS;
+
+namespace Catalog.Products.CreateProduct;
+
+
+
+public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price) : ICommand<CreateProductResult>;
+public record CreateProductResult(Guid Id);
+
+internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+{
+  public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+  {
+    throw new NotImplementedException();
+  }
+}
